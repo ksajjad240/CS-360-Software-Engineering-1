afd4f814a277f6a30683813c2a8b24cae61c2f8e
ksajjad240 <44227463+ksajjad240@users.noreply.github.com>
2024-05-23 23:46:46 -0700

    client.js
    client.js

---
+++ Assignment8/client.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+
+const _USER_BASE_URL = 'http://localhost:3000/analytics';
+const _PAYMENT_BASE_URL = 'http://localhost:3000/analytics/paymentAnalytics';
+
+// Function to get analytics for all users
+async function getAllUserAnalytics() {
+    try {
+        const response = await axios.get(_USER_BASE_URL);
+        console.log('All User Analytics:', response.data);
+    } catch (error) {
+        console.error('Error fetching all user analytics:', error.message);
+    }
+}
+
+// Function to get analytics for a specific user
+async function getUserAnalytics(userId) {
+    try {
+        const response = await axios.get(`${_USER_BASE_URL}/${userId}`);
+        console.log(`Analytics for User ${userId}:`, response.data);
+    } catch (error) {
+        console.error(`Error fetching analytics for user ${userId}:`, error.message);
+    }
+}
+
+// Function to get analytics for a specific user
+async function getLoginTrendAnalytics(userId) {
+    try {
+        const response = await axios.get(`${_USER_BASE_URL}/${userId}`);
+        console.log(`Login Trend Analytics for User ${userId}:`, response.data);
+    } catch (error) {
+        console.error(`Error fetching analytics for user ${userId}:`, error.message);
+    }
+}
+
+// Function to update analytics for a specific user
+async function updateUserAnalytics(userId) {
+    try {
+        const response = await axios.put(`${_USER_BASE_URL}/${userId}`);
+        console.log(`Updated Analytics for User ${userId}:`, response.data);
+    } catch (error) {
+        console.error(`Error updating analytics for user ${userId}:`, error.message);
+    }
+}
+
+// Function to get user payment analytics for a specific user
+async function getUserPaymentAnalytics() {
+    try {
+        const response = await axios.get(`${_USER_BASE_URL}/paymentAnalytics/`);
+        console.log(`User Payment Analytics`, response.data);
+    } catch (error) {
+        console.error(`Error fetching payment analytics:`, error.message);
+    }
+}
+
+
+// Function to get realtime activity alert analytics for a specific user
+async function getRealTimeActivityAlertAnalytics() {
+    try {
+        const response = await axios.get(`${_USER_BASE_URL}/realTimeActivityAlertAnalytics/`);
+        console.log(`Realtime Activity Alert Analytics`, response.data);
+    } catch (error) {
+        console.error(`Error fetching realtime activity alert analytics:`, error.message);
+    }
+}
+
+// Function to get realtime activity alert analytics for a specific user
+async function getPersonalizedUsageAnalytics() {
+    try {
+        const response = await axios.get(`${_USER_BASE_URL}/personalizedUsageAnalytics/`);
+        console.log(`Personalized Usage Report Analytics`, response.data);
+    } catch (error) {
+        console.error(`Error fetching realtime activity alert analytics:`, error.message);
+    }
+}
+
+
+// Example usage
+getAllUserAnalytics();
+getUserAnalytics(1);
+getUserAnalytics(2);
+
+getLoginTrendAnalytics(1);
+getRealTimeActivityAlertAnalytics();
+getPersonalizedUsageAnalytics();
